d2f8a61028823e766e9e8f3717904b769321b8ce
PetarAD <petar.ad@mail.bg>
2016-04-17 13:40:22 +0300

    foot
    foot

--- modules/footer.php
+++ modules/footer.php
@@ -1,19 +1,22 @@
 <footer>
-  <nav class="footer-nav">
+  <nav class="footer-navigation">
     <ul>
-      <li><a href="#">Начало</a></li>
-      <li><a href="#">Начало</a></li>
-      <li><a href="#">Начало</a></li>
-      <li><a href="#">Начало</a></li>
-      <li><a href="#">Начало</a></li>
-      <li><a href="#">Начало</a></li>
+      <li><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i>
+  Начало</a></li>
+      <li><a href="category.php"><i class="fa fa-filter" aria-hidden="true"></i>
+  Категории</a></li>
+      <li><a href="create.php"><i class="fa fa-calendar" aria-hidden="true"></i>
+  Създай събитие</a></li>
+      <li><a href="admin.php"><i class="fa fa-lock" aria-hidden="true"></i>
+  Админ</a></li>
     </ul>
   </nav>
-  <ul class="social">
-    <li><a href="#">Facebook</a></li>
-    <li><a href="#">Twitter</a></li>
-    <li><a href="#">Pinterest</a></li>
-    <li><a href="#">Google plus</a></li>
+  <ul class="social flex-3">
+    <li><a href="#" target="_blank" class="facebook"></a></li>
+    <li><a href="#" target="_blank" class="twitter"></a></li>
+    <li><a href="#" target="_blank" class="google-plus"></a></li>
+    <li><a href="#" target="_blank" class="pinterest"></a></li>
   </ul>
+       </section>
   <p class="copy">Всички права запазени. <?= date('Y') ?></p>
 </footer>
